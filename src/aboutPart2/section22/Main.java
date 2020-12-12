dc15e5ce1a81efa5dfcd27a5927336a5f8f48891
ma22npk <gunjo64@icloud.com>
2020-12-12 23:12:55 +0900

    黒本第二章問２２の復習コード

---
+++ src/aboutPart2/section22/Main.java
@@ -0,0 +1,10 @@
+package aboutPart2.section22;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuilder sBuilder = new StringBuilder("あいうえお");
+        System.out.println(sBuilder.indexOf("いうえ"));
+    }
+
+}
